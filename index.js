601dfe084d217a7182194c1fd422f04eea460df8
Henry Savit <henrysavit@server.itshenry.com>
2015-12-08 18:13:19 -0500

    a

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express()
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server)
 
+app.set('port',  (process.env.PORT || 3000));
+
 server.listen(process.env.PORT || 3000);
 
 app.use(express.static(__dirname + "/"))
