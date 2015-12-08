8a2339eec145b7d5d40ca66c4ee0eed3748bb01e
Henry Savit <henrysavit@server.itshenry.com>
2015-12-08 17:16:37 -0500

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
+
+server.listen(process.env.PORT || 3000);
+
+console.log("http server listening on %d", port)
+
+io.sockets.on('connection', function(socket) { //'connection' or any other event
