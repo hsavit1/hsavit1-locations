1186a9b5291774a5102281582ff94a20b302d3e2
Henry Savit <henrysavit@server.itshenry.com>
2015-12-08 17:45:15 -0500

    a

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var io = require('socket.io').listen(server)
 
 server.listen(process.env.PORT || 3000);
 
-console.log("http server listening on %d", port)
+// console.log("http server listening on %d", port)
 
 io.sockets.on('connection', function(socket) { 
 
