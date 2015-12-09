0381aafa08825a76a8deafa3b57a122059d44885
Henry Savit <henrysavit@server.itshenry.com>
2015-12-09 14:14:41 -0500

    g

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-// var io = require('socket.io')(server, {
-//   serveClient: (config.env === 'production') ? false : true,
-//   path: '/socket.io'
-// });
+var io = require('socket.io')(server); 
 
 app.set('port',  (process.env.PORT || 3000));
 
@@ -21,23 +17,8 @@ app.get('/',  function (req, res) {
 io.on('connection', function(socket) { 
     console.log("Someone connected %s", socket.id);
 
-  // socket.on('join', function (data){
-  //   socket.room = data;
-  // })
-
-
-   // socket.emit('connection', {
-	  //  hello: 'world'
-   // });
-   
-   // socket.emit('stuff', function(other)
-   // {
-   //     console.log(other);
-   // });
-
     // respond on client message
     socket.on("client", function(data) {
-
         socket.emit("client", "Response from server on " + new Date().toTimeString() + " to " + data);
     });
 })
