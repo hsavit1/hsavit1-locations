3fd518874286cd4236c1f85f084cee7c90ccc505
Henry Savit <henrysavit@server.itshenry.com>
2015-12-09 12:45:39 -0500

    v

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ app.use(express.static("./public"));
 app.use(express.static(".//node_modules/socket.io-client/"));
 
 
-app.get('/',  function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/',  function (req, res) {
+//   res.send('Hello World!');
+// });
 
 io.set('log level', 2);
 
